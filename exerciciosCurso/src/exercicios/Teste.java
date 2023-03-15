cad040db2fbc6482759aa1210eb04420bc46a05c
DeevAlex <alexdematoscaceres@gmail.com>
2023-03-15 15:39:13 -0300

    removido

--- exerciciosCurso/src/exercicios/Teste.java
+++
@@ -1,9 +0,0 @@
-package exercicios;
-
-public class Teste {
-
-	public static void main(String[] args) {
-		System.out.println("a");
-	}
-	
-}
--- exerciciosPOO/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author alexd
- *
- */
-module ExerciciosPOO {
-}
